e1b8543c30024e23ddbc5ca2efcd986a52bf4b32
Lior <liorsl@protonmail.com>
2020-10-27 15:58:27 +0200

    Trying to fix a bug

--- מחולל צוות תוכן_files/scriptMain.js
+++ מחולל צוות תוכן_files/scriptMain.js
@@ -285,6 +285,7 @@ function itsGaming() {
 }
 
 function loadAssets() {
+    console.log("assetsDir" + assetsDir)
     document.getElementById("header").innerHTML = $.get(assetsDir + "header.html")
     document.getElementById("dept-select").innerHTML = `
          <center>
@@ -293,6 +294,7 @@ function loadAssets() {
     `
 
     $("rules-insert").innerHTML = $.get(assetsDir + "rules.html")
+
     //document.getElementsByClassName("rules").item(0).innerHTML = $.get(assetsDir + "rules.html")
 }
 
