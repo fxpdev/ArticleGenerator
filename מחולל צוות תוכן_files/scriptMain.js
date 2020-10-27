32867e8c7a5deee091f9c89b807bf37b8f6f570c
Lior <liorsl@protonmail.com>
2020-10-27 16:29:19 +0200

    Remove tech update forums from gaming generator

--- מחולל צוות תוכן_files/scriptMain.js
+++ מחולל צוות תוכן_files/scriptMain.js
@@ -262,9 +262,19 @@ function itsTech() {
         $("#gamingOnly2").hide(),
         $("#onlygaming3").hide(),
         document.getElementById("relvHelpBox").innerHTML = "קישור רלוונטי לכתבה, לדוגמה: אתר הנושא, קישור לקניית הפריט טכנולוגייה, בלוג על הנושא.<br />שימו לב שהמחולל מוסיף בעצמו נקודתיים בסוף התיאור!<br />בתיבה הימנית הכניסו את תיאור הכתבה ובתיבה השמאלית את הקישור עצמו.";
+
     document.getElementById("updatesforums").innerHTML = ' מלאו את הכתבות האחרונות מאחד מפורומי העדכונים: ',
         $.get("SelectTech.txt", EmbedSelect, "text")
 
+    document.getElementById("updatesforums2").innerHTML = `
+                                            <a href="https://www.fxp.co.il/forumdisplay.php?f=4607" class="dept">עדכוני
+                                        מולטימדיה,</a> <a href="https://www.fxp.co.il/forumdisplay.php?f=4603"
+                                                          class="dept">עדכוני
+                                        אנדרואיד,</a> <a href="https://www.fxp.co.il/forumdisplay.php?f=4602"
+                                                         class="dept">עדכוני
+                                        Apple,</a> <a href="https://www.fxp.co.il/forumdisplay.php?f=5073" class="dept">עדכוני
+                                        מחשבים וחומרה</a>:
+`
     //document.getElementById("dept-gaming").src = "images/dept-tech.png",
     //document.getElementById("dept-gaming").alt = "מחלקת טכנלוגיה",
 
@@ -285,10 +295,6 @@ function itsGaming() {
 }
 
 function loadAssets() {
-    console.log("assetsDir " + assetsDir)
-
-    //document.getElementById("header").innerHTML = $.get(assetsDir + "header.html")
-
 
     $.get(assetsDir + "header.html", function (data) {
         console.log("header.html")
