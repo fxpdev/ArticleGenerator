4b3f5325ce8d964f5abc4622ab1f517585d9106d
Lior <liorsl@protonmail.com>
2020-10-27 17:06:32 +0200

    Fix bug with relevant forums (?)

--- מחולל צוות תוכן_files/scriptMain.js
+++ מחולל צוות תוכן_files/scriptMain.js
@@ -263,8 +263,8 @@ function itsTech() {
         $("#onlygaming3").hide(),
         document.getElementById("relvHelpBox").innerHTML = "קישור רלוונטי לכתבה, לדוגמה: אתר הנושא, קישור לקניית הפריט טכנולוגייה, בלוג על הנושא.<br />שימו לב שהמחולל מוסיף בעצמו נקודתיים בסוף התיאור!<br />בתיבה הימנית הכניסו את תיאור הכתבה ובתיבה השמאלית את הקישור עצמו.";
 
-    document.getElementById("updatesforums").innerHTML = ' מלאו את הכתבות האחרונות מאחד מפורומי העדכונים: ',
-        $.get("SelectTech.txt", EmbedSelect, "text")
+    /*document.getElementById("updatesforums").innerHTML = ' מלאו את הכתבות האחרונות מאחד מפורומי העדכונים: ',
+        $.get("SelectTech.txt", EmbedSelect, "text")*/
 
     document.getElementById("updatesforums2").innerHTML = `
                                             <a href="https://www.fxp.co.il/forumdisplay.php?f=4607" class="dept">עדכוני
