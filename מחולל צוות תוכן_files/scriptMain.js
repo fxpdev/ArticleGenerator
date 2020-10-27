3483723b16b3bf76b4f6d3c9b61c5739baffb787
Lior <liorsl@protonmail.com>
2020-10-27 16:10:09 +0200

    Trying to fix a bug

--- מחולל צוות תוכן_files/scriptMain.js
+++ מחולל צוות תוכן_files/scriptMain.js
@@ -295,7 +295,7 @@ function loadAssets() {
 
     console.log("setting dept rules")
     console.log($.get(assetsDir + "rules.html").responseText)
-    document.getElementsByClassName("dept-rules").innerHTML = `
+    document.getElementById("dept-rules").innerHTML = `
                 <section class="section1">
                 <table class="detailstable">
                     <tbody><tr>
